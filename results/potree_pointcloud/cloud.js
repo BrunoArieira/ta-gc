9f65f2d0d0a03a01123645f39c2c86aff7a7bd43
BrunoArieira <a70565@alunos.uminho.pt>
2019-04-16 11:53:48 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 15039246,
+    "boundingBox": {
+        "lx": 554033.86,
+        "ly": 4505756.78,
+        "lz": -18.46,
+        "ux": 554449.45,
+        "uy": 4506172.37,
+        "uz": 397.1299999999674
+    },
+    "tightBoundingBox": {
+        "lx": 554033.86,
+        "ly": 4505756.78,
+        "lz": -18.46,
+        "ux": 554449.45,
+        "uy": 4506092.600000001,
+        "uz": 61.67
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.8792920112609865,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
